b9dc9b61b65012d8bbc00e8e13c058465cf74cbd
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2023-11-01 22:28:06 +0200

    add options for websocket

--- socket.ts
+++ socket.ts
@@ -1,13 +1,13 @@
 import {hHelpers, hono} from "./deps.ts"
 
-export const useSocket = (handler: (socket: WebSocket, c: hono.Context) => void) => {
+export const useSocket = (handler: (socket: WebSocket, c: hono.Context) => void, options?: Deno.UpgradeWebSocketOptions) => {
   return hHelpers.createMiddleware(async (c, next) => {
     if (c.req.header('upgrade') !== 'websocket') {
       await next()
       return
     }
 
-    const {socket, response} = Deno.upgradeWebSocket(c.req.raw)
+    const {socket, response} = Deno.upgradeWebSocket(c.req.raw, options)
     handler(socket, c)
     return response
   })
