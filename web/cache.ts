a294460826c14c332c820f1f218dec623378d44d
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2025-01-06 19:45:51 +0200

    fix: supported 'Age' header for Deno Deploy Cache

--- web/cache.ts
+++ web/cache.ts
@@ -1,10 +1,26 @@
-export type CacheOptions = {
-  /** `cacheName` */
+export interface CacheOptions {
+  /**
+   * The name of the cache.
+   */
   name: string
-  /** @default false */
+
+  /**
+   * Whether to log cache operations.
+   * @default false
+   */
   log?: boolean
-  /** `Seconds` */
+
+  /**
+   * Time to live for cached items in seconds.
+   * @default 0
+   */
   ttl?: number
+
+  /**
+   * Whether to force delete expired cache items.
+   * @default false
+   */
+  forceDelete?: boolean
 }
 
 export const createCachedFetch = async (options: CacheOptions) => {
@@ -22,23 +38,38 @@ export const createCachedFetch = async (options: CacheOptions) => {
     // find in cache
     const matches = await cache.match(reqInit)
     if (matches) {
+      const age = matches.headers.get('Age')
       const cacheControl = matches.headers.get('Cache-Control')
-      if (cacheControl) {
+
+      // Check if Age header is present and greater than TTL
+      if (age) {
+        if (parseInt(age, 10) > CACHE_TTL) {
+          if (options.log) console.log('[cache]%c Age header is greater than TTL', 'color: red')
+        } else {
+          if (options.log) console.log('[cache]%c Age header is within TTL', 'color: green')
+          return matches
+        }
+      } else if (cacheControl) {
         const maxAgeMatch = cacheControl.match(/max-age=(\d+)/)
         if (maxAgeMatch) {
           const maxAge = parseInt(maxAgeMatch[1], 10)
           const cachedTime = Date.parse(matches.headers.get('Date') || '')
           if (!isNaN(cachedTime) && Date.now() - cachedTime < maxAge * 1000) {
-            if (options.log) console.log('[cache] matched', matches.url)
+            if (options.log) console.log('[cache]%c hit', 'color: green')
             return matches
+          } else {
+            if (options.log) console.log('[cache]%c expired', 'color: red')
           }
         }
       }
 
-      // delete expired
-      cache.delete(reqInit).then((v) => {
-        if (options.log && v) console.log('[cache] delete', reqInit.url, reqInit)
-      })
+      if (options.log) console.log(`[cache]%c miss`, 'color: orange')
+      if (options.forceDelete) {
+        // delete expired
+        cache.delete(reqInit).then((v) => {
+          if (options.log && v) console.log(`[cache]%c delete ${reqInit.url}`, 'color: red')
+        })
+      }
     }
 
     // direct
@@ -48,7 +79,7 @@ export const createCachedFetch = async (options: CacheOptions) => {
       const responseToCache = new Response(resClone.body, resClone)
       responseToCache.headers.set('Cache-Control', `max-age=${CACHE_TTL}`)
       await cache.put(reqInit, responseToCache)
-      if (options.log) console.log('[cache] put', res.status, reqInit.url)
+      if (options.log) console.log(`[cache]%c put ${res.status} ${reqInit.url}`, 'color: green')
     }
 
     return res
